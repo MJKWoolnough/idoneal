b4c57e3cf61a4dbb7317c3d6fbc6d74fb6ba8c1b
Michael Woolnough <michael.woolnough@gmail.com>
2023-02-23 19:34:39 +0000

    added package + func comments

--- pkg/meta/sequences.go
+++ pkg/meta/sequences.go
@@ -1,3 +1,5 @@
+// The meta package provides some simple convenience functions for dealing with
+// FASTQ files.
 package meta
 
 import (
@@ -5,6 +7,11 @@ import (
 	"io"
 )
 
+// CountSequences returns the number of Sequences in the passed FASTQ formatted
+// Reader.
+//
+// NB: This function acts naively, simply counting the non-blank lines in the
+// Reader, and diving by 4.
 func CountSequences(r io.Reader) (int, error) {
 	lr := cleaner(r)
 	var buf [1024]byte
